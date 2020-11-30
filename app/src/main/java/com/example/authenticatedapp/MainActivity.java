b5f6959891c256b6236d4577da70f95f79d1562b
Bindu Bindu <bindutiwana786@gmail.com>
2020-11-30 17:09:15 -0500

    first commit

---
+++ app/src/main/java/com/example/authenticatedapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.authenticatedapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.core.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+
+
+    public void logout(android.view.View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(),Login.class));
+        finish();
+    }
+}
